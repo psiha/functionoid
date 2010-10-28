a7d428d080eff11d0fdc345cd1bacb452ef8aba1
psiha <psiha@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2010-10-28 12:22:10 +0000

    Added new file headers.
    git-svn-id: http://svn.boost.org/svn/boost/sandbox/function@66225 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/function/function_template.hpp
+++ boost/function/function_template.hpp
@@ -1,12 +1,21 @@
-// Boost.Function library
-
-//  Copyright Douglas Gregor 2001-2006
-//  Copyright Emil Dotchevski 2007
-//  Use, modification and distribution is subject to the Boost Software License, Version 1.0.
-//  (See accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-
-// For more information, see http://www.boost.org
+////////////////////////////////////////////////////////////////////////////////
+///
+/// Boost.Function library
+/// 
+/// \file function_template.hpp
+/// ---------------------------
+///
+///  Copyright (c) Douglas Gregor  2001-2006
+///  Copyright (c) Emil Dotchevski 2007
+///  Copyright (c) Domagoj Saric   2010
+///
+///  Use, modification and distribution is subject to the Boost Software
+///  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt)
+///
+/// For more information, see http://www.boost.org
+///
+////////////////////////////////////////////////////////////////////////////////
 
 // Note: this header is a header template and must NOT have multiple-inclusion
 // protection.
