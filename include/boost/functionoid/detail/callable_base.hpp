ec954459af1f9cbf3696b7c5772e45328e0b211a
Luka Mate Granić <luka.granic@microblink.com>
2020-11-27 14:55:11 +0100

    Clang: added a workaround for v12 crashes.

--- include/boost/functionoid/detail/callable_base.hpp
+++ include/boost/functionoid/detail/callable_base.hpp
@@ -538,7 +538,10 @@ using functor_manager = typename functor_manager_aux
 /// are used (instead of const pointers) by generating/storing 'null
 /// references'.
 ///                                       (14.10.2016.) (Domagoj Saric)
-#if BOOST_WORKAROUND( BOOST_MSVC, BOOST_TESTED_AT( 1900 ) ) || ( defined( __clang__ ) && __clang_major__ <= 7 )
+/// \note Clang struggles...
+/// https://github.com/emscripten-core/emscripten/issues/12639#issuecomment-719235628
+///                                       (30.11.2020.) (Domagoj Saric)
+#if BOOST_WORKAROUND( BOOST_MSVC, BOOST_TESTED_AT( 1900 ) ) || ( defined( __clang__ ) && ( __clang_major__ <= 7 || __clang_major__ == 12 ) )
 #define BOOST_AUX_NOEXCEPT_PTR( condition )
 #else
 #define BOOST_AUX_NOEXCEPT_PTR( condition ) noexcept( condition )
